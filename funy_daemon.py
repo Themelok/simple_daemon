9f31edc86b87042f84a7a85fc701c9ae50eaa08c
themelok <3038686@gmail.com>
2017-04-22 20:34:20 +0300

    200 ok

--- funy_daemon.py
+++ funy_daemon.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-
 import MySQLdb
 import syslog
 import time
 from configparser import ConfigParser
 from urllib.request import urlopen
 from xml.etree import ElementTree as et
-from xml.dom.minidom import parse
 
 parser = ConfigParser()
 parser.read('/home/sysad/MEGA/tmp/ot/fd.conf')
@@ -14,13 +12,6 @@ db_con = {}
 items = parser.items('mysql')
 for item in items:
     db_con[item[0]] = item[1]
-
-#
-# cursor = db.cursor()
-# sql = "INSERT INTO cd VALUES ('Empire Burlesque', 'Bob Dylan', '10.90', '1985');"
-# cursor.execute(sql)
-# db.commit()
-# db.close()
 
 urlmy = parser.sections()[0]
 delay = int(parser[urlmy]['delay'])
@@ -64,8 +55,8 @@ class ApptParser:
             cursor = self.db.cursor()
             cd_list = self.xml_data.findall('CD')
             for x in cd_list:
-                TITLE = x.find('TITLE').text.replace("'","")
-                ARTIST = x.find('ARTIST').text.replace("'","")
+                TITLE = x.find('TITLE').text.replace("'", "")
+                ARTIST = x.find('ARTIST').text.replace("'", "")
                 PRICE = x.find('PRICE').text
                 YEAR = x.find('YEAR').text
                 sql = "INSERT INTO cdcatalog VALUES ('{}', '{}', '{}', '{}');".format(
